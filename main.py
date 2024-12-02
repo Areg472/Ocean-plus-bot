39d21da37f1abb852263a5ecb3d92ae3aaf5dd59
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-02 17:58:48 +0400

    hmmm

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import json
 import datetime
 import os
+
+from discord.app_commands import describe
 from discord.ext import commands
 from keep_alive import keep_alive
 
@@ -31,16 +33,16 @@ async def on_ready():
     await bot.tree.sync()
     print(f'Logged in as {bot.user}!')
 
-@bot.tree.command(name="hi")
+@bot.tree.command(name="hi", description="Say hi to the bot!")
 async def hi(interaction: discord.Interaction):
     await interaction.response.send_message('Hi. I am the official Ocean+ discord bot!')
 
-@bot.tree.command(name="meme")
+@bot.tree.command(name="meme", description="Send a funny meme!")
 async def meme(interaction: discord.Interaction):
     meme_url = get_meme()
     await interaction.response.send_message(meme_url)
 
-@bot.tree.command(name="date")
+@bot.tree.command(name="date", description="Get the current date and days until the next Ocean+ anniversary!")
 async def date(interaction: discord.Interaction):
     await interaction.response.send_message(f'Today is {today}!\nThere are {days_until_oplus} days until the next Ocean+ anniversary!')
 

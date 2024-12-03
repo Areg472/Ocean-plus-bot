c4be7ca62534d437a8d4fc92fe84d6919ae57275
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-03 11:35:45 +0400

    hmmm

--- main.py
+++ main.py
@@ -111,7 +111,11 @@ async def duck(interaction: discord.Interaction):
 async def dad_joke(interaction: discord.Interaction):
     response = requests.get("https://api.popcat.xyz/joke")
     json_data = response.json()
-    await interaction.response.send_message(f"{json_data['joke']}")
+    nothing = random.randint(1, 20)
+    if nothing == 1:
+        await interaction.response.send_message('"Why does Areg break this bot a lot? Because he is broken."')
+    else:
+        await interaction.response.send_message(f"\"{json_data['joke']}\"")
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)

108da10bed9c1f3b6fbd29cda92aac81bc02d32e
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-06 19:06:36 +0400

    weather!

--- main.py
+++ main.py
@@ -338,8 +338,8 @@ async def wanted(interaction: discord.Interaction, person: discord.User):
     await interaction.response.send_message(response.url)
 
 @app_commands.context_menu(name="Grammar")
-async def grammar(interaction: discord.Interaction, message: discord.Message.content):
-    text = str(message)
+async def grammar(interaction: discord.Interaction, message: discord.Message):
+    text = str(message.content)
     suggest = tool.correct(text)
     await interaction.response.send_message(f"Errm did you mean {suggest}?")
 

48796c5dedd7f19c85b9875ab940a83fc883ccf8
Areg Azatyan <74340447+Areg472@users.noreply.github.com>
2025-06-15 12:58:41 +0000

    nothing

--- main.py
+++ main.py
@@ -56,14 +56,6 @@ async def on_ready():
     print(f'Logged in as {bot.user}!')
     channel = bot.get_channel(1335634555377291306)
     await channel.send("Tim hortons is the best!")
-async def on_message(message: discord.Message):
-    if (
-        message.channel.id == 1335634555377291306
-        and message.content.strip().lower() == "opluswhiplashkim"
-    ):
-        await message.channel.send(
-            "<@1299815086147502080> https://gr5mutu1hr.ufs.sh/f/thKihuQxhYcPirR9qkuwXSxsTe0NZrlH9R3WGDJCUcgj2YvB"
-        )
 
 def cooldown(interaction: discord.Interaction):
     return app_commands.Cooldown(1, 3.0)
@@ -492,6 +484,14 @@ MAX_HISTORY = 30
 async def on_message(message: discord.Message):
     if message.author.bot:
         return
+
+    if (
+        message.channel.id == 1335634555377291306
+        and message.content.strip().lower() == "opluswhiplashkim"
+    ):
+        await message.channel.send(
+            "<@1299815086147502080> https://gr5mutu1hr.ufs.sh/f/thKihuQxhYcPirR9qkuwXSxsTe0NZrlH9R3WGDJCUcgj2YvB"
+        )
 
     if message.channel.id != 1315586087573258310:
         return

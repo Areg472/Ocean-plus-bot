87e3832a55c748d726614bf047e3018466b188c0
Areg Azatyan <74340447+Areg472@users.noreply.github.com>
2025-06-15 05:15:51 +0000

    pet fix

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ async def help(interaction: discord.Interaction):
         ("Context menu command - Spelling Checker", "Check your spelling!"),
         ("/gamble", "Randomly gamble!"),
         ("/wikipedia", "Search wikipedia articles"),
-        ("/pat", "Pat the mentioned user!"),
+        ("/pet", "Pet the mentioned user!"),
         ("/jail", "Put the mentioned user in jail!"),
         ("/github", "Get github info of a user"),
         ("/joke_overhead", "Use this and mention the guy that doesn't understand jokes!"),
@@ -624,10 +624,10 @@ async def bonk(interaction: discord.Interaction, person: discord.User):
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-@bot.tree.command(name="pat", description="Pat the mentioned user!")
-@app_commands.describe(person="The person you want to pat")
+@bot.tree.command(name="pet", description="Pet the mentioned user!")
+@app_commands.describe(person="The person you want to pet")
 @app_commands.checks.dynamic_cooldown(cooldown)
-async def pat(interaction: discord.Interaction, person: discord.User):
+async def pet(interaction: discord.Interaction, person: discord.User):
     if not jeyy_api:
         await interaction.response.send_message("API key not configured!")
         return

a046a9dae56eb601b576c9f34b1ebb8e1bcea32c
Aregus <74340447+Areg472@users.noreply.github.com>
2025-07-31 12:25:46 +0400

    more fixes

--- commands/prompt.py
+++ commands/prompt.py
@@ -24,14 +24,14 @@ def setup(bot):
 
 
 MODEL_CHOICES = [
+    app_commands.Choice(name="Voxtral Mini", value="voxtral-mini-2507"),
     app_commands.Choice(name="Mistral Small", value="mistral-small-2506"),
     app_commands.Choice(name="Devstral Small", value="devstral-small-2507"),
+    app_commands.Choice(name="Voxtral Small", value="voxtral-small-2507"),
     app_commands.Choice(name="Qwen 3 235B", value="Qwen/Qwen3-235B-A22B-Instruct-2507-tput"),
     app_commands.Choice(name="Magistral Small", value="magistral-small-2507"),
     app_commands.Choice(name="Mistral Medium", value="mistral-medium-2505"),
     app_commands.Choice(name="DeepSeek R1", value="deepseek-ai/DeepSeek-R1-0528-tput"),
-    app_commands.Choice(name="Voxtral Mini", value="voxtral-mini-2507"),
-    app_commands.Choice(name="Voxtral Small", value="voxtral-small-2507"),
 ]
 
 @app_commands.allowed_installs(guilds=True, users=True)

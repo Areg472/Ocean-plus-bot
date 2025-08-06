8cf740812401412ec1c0aa33c244286f55bc5ceb
Aregus <74340447+Areg472@users.noreply.github.com>
2025-08-06 11:10:27 +0400

    JS lol

--- commands/utils.py
+++ commands/utils.py
@@ -144,7 +144,7 @@ async def handle_api_call_stream(prompt: str, instructions: str = "", timeout: i
                     model=model,
                     instructions=instructions,
                 )
-                print(response);
+                print(response)
                 response_text = ""
                 for event in response:
                     try:
